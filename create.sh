f7a77577d83da8aea941ca5ea3a4d2045cad0b38
Sean Turner <sean.turner026@gmail.com>
2023-04-08 15:54:33 -0400

    Not using helm

--- create.sh
+++ create.sh
@@ -1,23 +1,66 @@
+#!/bin/bash
+
+set -x
+set -o errexit
+set -o pipefail
+set -o nounset
+
 # Create k8s cluster with Kind.
-kind create cluster --name local
+kind create cluster --name local --config kind.yaml
 
 # Install ArgoCD.
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl port-forward svc/argocd-server -n argocd 8080:443
 
-PASSWORD=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 --decode)
-argocd login localhost:8080 --username admin --password $PASSWORD
+kubectl patch svc argocd-server -n argocd -p \
+'{
+    "spec": {
+        "type": "NodePort",
+        "ports": [
+            {
+                "name": "http",
+                "nodePort": 30080,
+                "port": 80,
+                "protocol": "TCP",
+                "targetPort": 8080
+            }, {
+                "name": "https",
+                "nodePort": 30443,
+                "port": 443,
+                "protocol": "TCP",
+                "targetPort": 8080
+            }
+        ]
+    }
+}'
+
+set +o errexit
+set +o pipefail
+
+for i in {1..10}; do
+    PASSWORD=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 --decode)
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 1
+done
+
+set -o errexit
+set -o pipefail
+
+argocd login localhost:8080 --username admin --password $PASSWORD --insecure
 argocd cluster add kind-local --yes
+
+mkdir -p .tmp
+envsubst < applications/renovate/secret.yaml > .tmp/secret.yaml
 
 # Configure Renovate pre-resources.
 kubectl create namespace renovate
-kubectl apply -f applications/renovate/secret.yaml
-kubectl edit secret renovate -n renovate
+kubectl apply -f .tmp/secret.yaml
 
 # Create ArgoCD Applications.
 argocd app create cluster-applications \
     --repo https://github.com/seanturner026/argocd-renovate.git \
     --path applications \
-    --dest-namespace default \
+    --dest-namespace argocd \
     --dest-server https://kubernetes.default.svc
